9527f45b7e5fe9accbae7a8d2fc593a136d0118e
Nick Rabow <rabownick123@gmail.com>
2023-04-01 07:31:58 -0500

    Revert "Set up some custom ViewModifiers and started the initial work on a color pallet"
    This reverts commit b6d17fb80f99c1ff915c5479b46d312bc1b70347.

--- fife/ContentView.swift
+++ fife/ContentView.swift
@@ -8,73 +8,32 @@
 import SwiftUI
 import CoreData
 
-struct FifeColor {
-    
-    static let backgroundColor = Color(red: 23 / 255, green: 21 / 255, blue: 25 / 255)
-    static let pink = Color(red: 255 / 255, green: 23 / 255, blue: 135 / 255)
-    static let blue = Color(red: 0 / 255, green: 155 / 255, blue: 242 / 255)
-}
-
-struct DefaultButtonViewModifier: ViewModifier {
-
-    @State var backgroundColor = Color.pink
-    
-    func body(content: Content) -> some View {
-        content
-            .font(.system(size:16, weight: .semibold))
-            .foregroundColor(.white)
-            .frame(height: 64)
-            .frame(maxWidth: .infinity)
-            .background(backgroundColor)
-            .cornerRadius(4)
-    }
-}
-
-extension View {
-    
-    func withDefaultButtonFormatting(backgroundColor: Color) -> some View {
-            modifier(DefaultButtonViewModifier(backgroundColor: backgroundColor))
-    }
-}
-
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     @State var number = ""
     
     var body: some View {
-        ZStack {
-            
-            FifeColor.backgroundColor.ignoresSafeArea()
-            
-            VStack(alignment: .leading, spacing: 8) {
-                
-                Text("Log in using")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                
-                Button() {
-                    goToPhoneVerification()
-                } label: {
-                    Label("Phone Number", systemImage: "iphone")
-                }
-                .withDefaultButtonFormatting(backgroundColor: FifeColor.pink)
-                
-                Button {} label: {
-                    Label("Email", systemImage: "mail")
-                }
-                .withDefaultButtonFormatting(backgroundColor: FifeColor.blue)
-                
+        VStack(alignment: .leading, spacing: 40) {
+           Text("Log in using")
+            Button() {
+                goToPhoneVerification()
+            } label: {
+                Label("Phone Number", systemImage: "iphone")
             }
-            .padding()
+            Button {} label: {
+                Label("Email", systemImage: "mail")
+            }
+
         }
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.roundedRectangle(radius: 8))
     }
     
     private func goToPhoneVerification() {
         
     }
 }
-    
 
 
 struct ContentView_Previews: PreviewProvider {
